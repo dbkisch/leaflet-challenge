6f2ef2b08c87ebbb9ffe8865c8be0b674d1ea0fc
dbkisch <kischdeb@gmail.com>
2025-04-05 13:55:05 -0500

    map with color/size markers working

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,4 +1,7 @@
 // Create the 'basemap' tile layer that will be the background of our map.
+let basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
 
 
 // OPTIONAL: Step 2
@@ -6,9 +9,13 @@
 
 
 // Create the map object with center and zoom options.
-
+let myMap = L.map("map", {
+  center: [44.977753, -93.265015],
+  zoom: 2
+});
 
 // Then add the 'basemap' tile layer to the map.
+basemap.addTo(myMap);
 
 // OPTIONAL: Step 2
 // Create the layer groups, base maps, and overlays for our two sets of data, earthquakes and tectonic_plates.
@@ -18,59 +25,78 @@
 // Make a request that retrieves the earthquake geoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
 
+  console.log(data);
+
+
   // This function returns the style data for each of the earthquakes we plot on
   // the map. Pass the magnitude and depth of the earthquake into two separate functions
   // to calculate the color and radius.
   function styleInfo(feature) {
-
+    return {
+      color: "black",
+      weight: .25,
+      fillOpacity: 5,
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      radius: getRadius(feature.properties.mag) * 3
+    }
   }
-
   // This function determines the color of the marker based on the depth of the earthquake.
   function getColor(depth) {
-
+    if (depth >= 90) return "red";
+    else if (depth >= 70) return "orange";
+    else if (depth >= 50) return "lightSalmon";
+    else if (depth >= 30) return "gold";
+    else if (depth >= 10) return "greenYellow"
+    else return "lime";
   }
-
   // This function determines the radius of the earthquake marker based on its magnitude.
   function getRadius(magnitude) {
-
+    // return Math.sqrt(magnitude);
+    return magnitude;
   }
-
   // Add a GeoJSON layer to the map once the file is loaded.
   L.geoJson(data, {
-    // Turn each feature into a circleMarker on the map.
-    pointToLayer: function (feature, latlng) {
+     // Turn each feature into a circleMarker on the map.
+    pointToLayer: function (feature,latlng) {
 
+      return L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]);
     },
-    // Set the style for each circleMarker using our styleInfo function.
+  //   // Set the style for each circleMarker using our styleInfo function.
     style: styleInfo,
-    // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
-    onEachFeature: function (feature, layer) {
-
+  //   // Create a popup for each marker to display the magnitude and location of the earthquake after the marker has been created and styled
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(`<h3>Magnitude: ${feature.properties.mag}</h3><hr><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
     }
+  
   // OPTIONAL: Step 2
   // Add the data to the earthquake layer instead of directly to the map.
-  }).addTo(map);
+  }).addTo(myMap);
 
   // Create a legend control object.
   let legend = L.control({
     position: "bottomright"
   });
-
+  
   // Then add all the details for the legend
   legend.onAdd = function () {
     let div = L.DomUtil.create("div", "info legend");
 
     // Initialize depth intervals and colors for the legend
-
-
+    let limits = ["-10-10","10-30","30-50","50-70","70-90","90+"];
+    let colors = ["red","orange","lightSalmon","gold","greenYellow","lime"];
+    let labels = [];
+  
     // Loop through our depth intervals to generate a label with a colored square for each interval.
+    limits.forEach(function(limit, index) {
+      labels.push("<li> ${limit[index]} ${colors[index]} </li>");
+    });
 
-
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
     return div;
   };
 
   // Finally, add the legend to the map.
-
+  legend.addTo(myMap);
 
   // OPTIONAL: Step 2
   // Make a request to get our Tectonic Plate geoJSON data.
